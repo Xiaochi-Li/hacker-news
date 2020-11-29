2ab5e841b697af1a5666d08951e9cf0f3069afc6
Xiaochi <294188719@qq.com>
2020-11-29 14:13:01 +1000

    impr:increase test coverage of ResultsContext.js

--- src/context/__test__/ResultsContext.test.js
+++ src/context/__test__/ResultsContext.test.js
@@ -4,23 +4,22 @@ import {
   RESPONSE_COMPLETE,
   ERROR,
 } from '../ResultsContext';
-
-const state = {
-  hits: null,
-  loading: false,
-  error: null,
-};
+import { mockState } from '../../utils/testHelper/mockState';
+import { mockHits } from '../../utils/testHelper/mockHits';
 
 describe('test fetchReducer', () => {
-  it('has correct output for LOADING action', () => {
+  it('should turn loading to true', () => {
     const action = { type: LOADING };
-    const ouput = fetchReducer(state, action);
-    expect(ouput.loading).toBeTruthy();
+    expect(fetchReducer(mockState, action).loading).toBeTruthy();
   });
 
-  it('has correct output for RESPONSE_COMPLETE action', () => {
-    const action = { type: RESPONSE_COMPLETE, payload: { hits: {} } };
-    const ouput = fetchReducer(state, action);
-    expect(ouput.loading).toBeTruthy();
+  it('should add hits to state', () => {
+    const action = { type: RESPONSE_COMPLETE, payload: { hits: mockHits } };
+    expect(fetchReducer(mockState, action).hits.length).toEqual(10);
+  });
+
+  it('should have error message', () => {
+    const action = { type: ERROR, payload: { error: 'mock error' } };
+    expect(fetchReducer(mockState, action).error).toEqual('mock error');
   });
 });
